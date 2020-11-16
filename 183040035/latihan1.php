3b894f5185ecd349be464bf97703e707e1a62f5d
Hadi Sutarma <Hadisutarma12@gmail.com>
2020-11-16 11:00:53 +0700

    Add pertemuan1 files

---
+++ 183040035/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+
+$mahasiwa = [
+    [
+        "nama" => "Shandika Galih",
+        "nrp" => "043040023",
+        "email" => "shandikagalih@unpas.ac.id"
+    ],
+    [
+        "nama" => "Erik Doank",
+        "nrp" => "023040001",
+        "email" => "erik@gmail.com"
+    ]
+];
+
+$data = json_encode($mahasiwa);
+echo $data;
---
+++ 183040035/script.js
@@ -0,0 +1,7 @@
+let mahasiswa = {
+    nama: "Sandhika Galih",
+    nrp: "030403023",
+    email: "Sandhikagalih@unpas.ac.id"
+}
+
+console.log(JSON.stringify(mahasiswa));
